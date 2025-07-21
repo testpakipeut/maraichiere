8b3db9a621bd8ae6f7a65edfadd815af9a1f2b6a
Zoumeye TOURE <ztoure@amexio.fr>
2025-07-21 22:03:14 +0200

    Connexion PDO adaptée pour Railway (host, port, user, password, dbname)

--- ferme-des-amandiers/data/db.php
+++ ferme-des-amandiers/data/db.php
@@ -1,17 +1,16 @@
 <?php
-// Paramètres de connexion à la base de données
-$host = 'localhost';
-$dbname = 'fermeDesAmandiers';
-$user = 'root';       // à adapter selon votre configuration
-$pass = 'admin';           // mot de passe, souvent vide sous XAMPP
+// Paramètres de connexion à la base de données Railway
+$host = 'maglev.proxy.rlwy.net';
+$port = 50295;
+$dbname = 'railway';
+$user = 'root';
+$pass = 'xdfPsRpVvZhiegOIJNFtgQRTggEPAIsj';
 
 try {
     // Connexion à la base via PDO
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
     // Activation des erreurs PDO (affichera les erreurs SQL en développement)
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
     // Message d’erreur si la connexion échoue
     die("Erreur de connexion à la base de données : " . $e->getMessage());
